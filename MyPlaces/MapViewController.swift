30b0a5e25d70be5e630210c85bb8f8fb34604188
D4rt-Dy14n <yuri.fedorov@inbox.ru>
2020-09-08 00:45:23 +0300

    Set up MapView and annotation view

---
+++ MyPlaces/MapViewController.swift
@@ -0,0 +1,80 @@
+//
+//  MapViewController.swift
+//  MyPlaces
+//
+//  Created by Юрий Федоров on 07.09.2020.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController {
+
+    var place: Place!
+    var annotationIdentifier = "annotationIdentifier"
+    @IBOutlet weak var MapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        MapView.delegate = self
+        setupPlaceMark()
+     
+    }
+
+    @IBAction func closeVC() {
+        dismiss(animated: true)
+    }
+
+    private func setupPlaceMark() {
+        guard let location = place.location else { return }
+        
+        let geocoder = CLGeocoder()
+        geocoder.geocodeAddressString(location) { (placemarks, error) in
+            if let error = error {
+                print(error.localizedDescription)
+                return
+            }
+            guard let placemarks = placemarks else { return }
+            
+            let placemark = placemarks.first
+            
+            let annotation = MKPointAnnotation()
+            annotation.title = self.place.name
+            annotation.subtitle = self.place.type
+            
+            guard let placemarkLocation = placemark?.location else { return }
+            
+            annotation.coordinate = placemarkLocation.coordinate
+            
+            self.MapView.showAnnotations([annotation], animated: true)
+            self.MapView.selectAnnotation(annotation, animated: true)
+        }
+        
+    }
+    
+}
+
+extension MapViewController: MKMapViewDelegate {
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        guard !(annotation is MKUserLocation) else { return nil}
+        
+        var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: annotationIdentifier) as? MKPinAnnotationView
+        
+        if annotationView == nil {
+            annotationView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: annotationIdentifier)
+            annotationView?.canShowCallout = true
+        }
+        if let imageData = place.imageData {
+            let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
+            imageView.layer.cornerRadius = 10
+            imageView.clipsToBounds = true
+            imageView.image = UIImage(data: imageData)
+            annotationView?.leftCalloutAccessoryView = imageView
+        }
+        
+        return annotationView
+    }
+    
+}
